14f7cccee7219d138bb85b966c149984c8e58add
gujabogve <gujabogve@gmail.com>
2019-03-19 13:52:33 +0400

    poi dependencies [add]

--- src/Models/Guest.java
+++ src/Models/Guest.java
@@ -1,9 +1,12 @@
 package Models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Guest {
     private String firstName, lastName, phone;
-
-    
+    private Integer age;
+    private List<Children> childrens = new ArrayList<>();
 
     public String getFirstName() {
         return firstName;
@@ -27,5 +30,25 @@ public class Guest {
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public List<Children> getChildrens() {
+        return childrens;
+    }
+
+    public void setChildrens(List<Children> childrens) {
+        this.childrens = childrens;
+    }
+
+    public void addChildren(Children children) {
+        this.childrens.add(children);
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 }
